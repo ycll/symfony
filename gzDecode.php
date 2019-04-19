cd4e4e597337e7aa59301186b7388013b3efd971
yincuilong <yincuilong@xiaozhu.com>
2019-04-19 17:33:53 +0800

    go

---
+++ gzDecode.php
@@ -0,0 +1,17 @@
+<?php
+//$url = 'http://affiliatefeed.agoda.com/datafeeds/feed/getfeed?feed_id=10&apikey=CC678C53-A150-4620-922F-F3E6C844A567&mHotel_id=168928';
+
+//curl_init();
+//curl_setopt(CURLOPT_URL, $url);
+
+//ob_start();
+//$res = ob_get_contents();
+//ob_end_clean();
+
+
+
+//var_dump($res);
+
+
+
+var_dump(gzencode('123'));
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,30 +1,1 @@
-<?php
-
-
-namespace App\Controller;
-
-
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class ArticleController
-{
-    /**
-     * @Route("/")
-     */
-    public function homepage()
-    {
-        return new Response('hello symfony');
-    }
-
-    /**
-     * @Route("/news/{slug}");
-     */
-    public function show($slug)
-    {
-        dump($slug, $this);
-        return new Response('<body>Future page to show one space article!</body>');
-    }
-
-}
+<?phpnamespace App\Controller;use Symfony\Component\HttpFoundation\JsonResponse;use Symfony\Component\HttpFoundation\Response;use Symfony\Component\Routing\Annotation\Route;class ArticleController{    /**     * @Route("/")     */    public function homepage()    {        return new Response('hello symfony');    }    /**     * @Route("/news/{slug}");     */    public function show($slug)    {        dump($slug, $this);        return new Response('<body>Future page to show one space article!</body>');    }    /**     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"post"})     */    public function toggleArticleHeart()    {        return new JsonResponse(['heart' => rand(5, 100)]);    }}
